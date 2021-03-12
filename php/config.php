b5bcdc9535dc38b27ecf0eca2fee88f950bda611
KamimuraAkito <74999848+KamimuraAkito@users.noreply.github.com>
2021-03-12 13:16:22 +0900

    Add files via upload

---
+++ php/config.php
@@ -0,0 +1,7 @@
+<?php 
+define('DB_HOST','localhost');
+define('DB_NAME','userDeta');
+define('DB_USER','hogeUser');
+define('DB_PASS','hogehoge');
+define('DSN', 'mysql:host=localhost;dbname=test_login');
+?>
